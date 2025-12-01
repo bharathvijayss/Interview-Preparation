c88916ffdd600647d06eed9e1e0660deb4d258a0
Bharath Vijay S S <ssbharathvijay@gmail.com>
2025-12-01 21:28:43 +0530

    react way to find and create root element and render the view

--- react/index.js
+++ react/index.js
@@ -1,4 +1,9 @@
+// const rootEle = document.getElementById("root");
+// const headingEle = document.createElement('h1');
+// headingEle.innerHTML = "Hello World from React!";
+// rootEle.appendChild(headingEle);
+
+const headingEle = React.createElement('h1', {}, 'Hello World from React!');
 const rootEle = document.getElementById("root");
-const headingEle = document.createElement('h1');
-headingEle.innerHTML = "Hello World from React!";
-rootEle.appendChild(headingEle);
+const root = ReactDOM.createRoot(rootEle);
+root.render(headingEle);
