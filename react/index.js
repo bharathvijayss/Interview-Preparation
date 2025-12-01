6c598bb2ebfe4179528716e5cc3477fa940e025d
Bharath Vijay S S <ssbharathvijay@gmail.com>
2025-12-01 10:11:12 +0530

    Without React - Modifying the DOM Element by adding a heading element dynamically

---
+++ react/index.js
@@ -0,0 +1,4 @@
+const rootEle = document.getElementById("root");
+const headingEle = document.createElement('h1');
+headingEle.innerHTML = "Hello World from React!";
+rootEle.appendChild(headingEle);
