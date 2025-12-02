11f67165f19d1fb523d90064e026cdf9b29a29f4
Bharath Vijay S S <ssbharathvijay@gmail.com>
2025-12-02 19:41:56 +0530

    Creating nested elements via React.createElement and adding props

--- react/index.js
+++ react/index.js
@@ -3,7 +3,17 @@
 // headingEle.innerHTML = "Hello World from React!";
 // rootEle.appendChild(headingEle);
 
-const headingEle = React.createElement('h1', {}, 'Hello World from React!');
+const container = React.createElement('div', { id: 'container' }, [
+    React.createElement('div', { id: 'inner-container-1', key: 'container-1' }, [
+        React.createElement('h1', { id: 'heading-1-1', key: 'h1-1' }, 'I am heading h1 of first container'),
+        React.createElement('h2', { id: 'heading-2-1', key: 'h2-1' }, 'I am heading h2 of first container')
+    ]),
+    React.createElement('div', { id: 'inner-container-2', key: 'container-2' }, [
+        React.createElement('h1', { id: 'heading-1-2', key: 'h1-2' }, 'I am heading h1 of second container'),
+        React.createElement('h2', { id: 'heading-2-2', key: 'h2-2' }, 'I am heading h2 of second container')
+    ])
+])
+
 const rootEle = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEle);
-root.render(headingEle);
+root.render(container);
