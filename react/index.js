dbd4fdf7804c2c222da4a57608eae34980a4d920
Bharath Vijay S S <ssbharathvijay@gmail.com>
2025-12-03 23:51:35 +0530

    Component Composition using Functional Components

--- react/index.js
+++ react/index.js
@@ -1,18 +1,33 @@
 import ReactDOM from 'react-dom/client';
 
-const container = (
-    <div id='container'>
+
+const HeadingComponent1 = () => {
+    return (
         <div id='inner-container-1'>
             <h1 id='heading-1-1' className='heading'>I am heading h1 of first container</h1>
             <h2 id='heading-2-1'>I am heading h2 of first container</h2>
         </div>
+    );
+}
+
+const HeadingComponent2 = () => {
+    return (
         <div id='inner-container-2'>
             <h1 id='heading-1-2' className='heading'>I am heading h1 of second container</h1>
             <h2 id='heading-2-2'>I am heading h2 of second container</h2>
         </div>
-    </div>
-);
+    );
+}
+
+const ContainerComponent = () => {
+    return (
+        <div id='container'>
+            <HeadingComponent1 />
+            <HeadingComponent2 />
+        </div>
+    );
+}
 
 const rootEle = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEle);
-root.render(container);
+root.render(<ContainerComponent />);
