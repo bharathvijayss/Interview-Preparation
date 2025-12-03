69cd27757e874d1738730ba0727d8072cea742e5
Bharath Vijay S S <ssbharathvijay@gmail.com>
2025-12-03 23:48:31 +0530

    using jsx

--- react/index.js
+++ react/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const container = React.createElement('div', { id: 'container' }, [
-    React.createElement('div', { id: 'inner-container-1', key: 'container-1' }, [
-        React.createElement('h1', { id: 'heading-1-1', key: 'h1-1' }, 'I am heading h1 of first container'),
-        React.createElement('h2', { id: 'heading-2-1', key: 'h2-1' }, 'I am heading h2 of first container')
-    ]),
-    React.createElement('div', { id: 'inner-container-2', key: 'container-2' }, [
-        React.createElement('h1', { id: 'heading-1-2', key: 'h1-2' }, 'I am heading h1 of second container'),
-        React.createElement('h2', { id: 'heading-2-2', key: 'h2-2' }, 'I am heading h2 of second container')
-    ])
-])
+const container = (
+    <div id='container'>
+        <div id='inner-container-1'>
+            <h1 id='heading-1-1' className='heading'>I am heading h1 of first container</h1>
+            <h2 id='heading-2-1'>I am heading h2 of first container</h2>
+        </div>
+        <div id='inner-container-2'>
+            <h1 id='heading-1-2' className='heading'>I am heading h1 of second container</h1>
+            <h2 id='heading-2-2'>I am heading h2 of second container</h2>
+        </div>
+    </div>
+);
 
 const rootEle = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEle);
